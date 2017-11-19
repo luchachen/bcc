22fcf0815f8cc7f8ced4019fceec3527af47bcae
Joel Fernandes <joelaf@google.com>
2017-11-18 21:46:20 -0800

    Fix remotefd bug
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- src/python/bcc/__init__.py
+++ src/python/bcc/__init__.py
@@ -349,6 +349,8 @@ class BPF(object):
         if self.libremote:
             remotefd = self.libremote.bpf_prog_load(prog_type, func_str, license_str,
                                       kern_version)
+        else:
+            remotefd = -1
 
         buffer_len = LOG_BUFFER_SIZE
         while True:
